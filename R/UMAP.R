3a3a523f33cca9c0f4c6f02521d437e11578221b
nderituc <nderituc@mail.gvsu.edu>
2022-11-20 22:49:27 -0500

    Deleted the umapgp.R file and instead updated our original UMAP.R file and other activities.

--- R/UMAP.R
+++ R/UMAP.R
@@ -1,85 +1,38 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-
-# Introduction
-Uniform Manifold Approximation and Projection (UMAP) is an algorithm for dimensional reduction.The package that will be developed is suited to process small data with approximately 1,000 data points.
-
-# Loading packages
-```{r}
-library(umap)
-library(tidyverse)
-library(magrittr)
-```
-
-# Importing the dataset
-```{r}
-data("iris")
-iris
-```
-
-# Visualizing the dataset
-```{r}
-ggplot(data = iris, aes(x = Petal.Length, y = Petal.Width))+
-  xlab("Petal Length")+
-  ylab("Petal Width") +
-  geom_point(aes(color = Species,shape=Species))+
-  geom_smooth(method='lm')+
-  ggtitle("Iris Petal Length vs Width")
-```
-
-# Perprocessing the data for umap
-```{r}
-# Adding a unique row ID
-iris <- iris %>%
-  mutate(ID=row_number())
-
-# Creating a dataframe with all categorical variables with the unique row ID
-
-iris_meta <- iris %>%
-  select(ID, Species)
-
-```
-
-# Performing UMAP
-```{r}
-set.seed(142)
-umap_fit <- iris %>%
-  select(where(is.numeric)) %>%
-  column_to_rownames("ID") %>%
-  scale() %>%
-  umap()
-umap_df <- umap_fit$layout %>%
-  as.data.frame()%>%
-  rename(UMAP1="V1",
-         UMAP2="V2") %>%
-  mutate(ID=row_number())%>%
-  inner_join(iris_meta, by="ID")
-```
-
-# UMAP Plot
-```{r}
-umap_df %>%
-  ggplot(aes(x = UMAP1,
-             y = UMAP2,
-             color = Species))+
-  geom_point()+
-  labs(x = "UMAP1",
-       y = "UMAP2",
-      subtitle = "UMAP plot")
-```
 
 
 
+#' @title package
+#' @description Computes a manifold approximation and projection
+#' @param data dataframe
+#' @param x variable
+#' @param col variable
+#' @import ggplot
+#' @return object of class umap, containing atleast a component with an embedding and a component with configuration settings
+#' @examples umapgp(ID,species,data=datasets::iris)
+#'
+#' @export
+umapgp<-function(x,col,data){
+  data_meta <- data %>%
+    select(x, col)
+  umap_fit <- data %>%
+    select(where(is.numeric)) %>%
+    column_to_rownames("x") %>%
+    scale() %>%
+    umap(n_neighbors=70,min_dist=0.3)
+  umap_df <- umap_fit$layout %>%
+    as.data.frame()%>%
+    rename(UMAP1="V1",
+           UMAP2="V2") %>%
+    mutate(x=row_number())%>%
+    inner_join(data_meta, by="x")
+
+  umapplot<-umap_df %>%
+    ggplot2(aes(x = UMAP1,
+                y = UMAP2,
+                color = col))+
+    geom_point()+
+    labs(x = "UMAP1",
+         y = "UMAP2",
+         subtitle = "UMAP plot")
+  return(umapplot)
+}
