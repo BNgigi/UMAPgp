eb963c6b98897955f54ebc1eece7753cabcc1e85
nderituc <nderituc@mail.gvsu.edu>
2022-12-04 23:05:32 -0500

    Added magittr and tidyverse packages in the description

--- R/UMAP.R
+++ R/UMAP.R
@@ -5,7 +5,7 @@
 #' @description Computes Uniform Manifold Approximation and Projection (UMAP) while preserving global structures.
 #' @param data The dataset to be used; it should be a dataframe with no rownames.
 #' @param var A variable of interest from the dataset; it should be categorical.
-#' @import ggplot2 dplyr umap datasets  devtools  tibble
+#' @import ggplot2 dplyr umap datasets  devtools  tibble magrittr tidyverse
 #' @return  object of class umap, containing atleast a component with an embedding and a component with configuration settings
 #' @examples umapgp(var="Species",data=iris)
 #' @export
